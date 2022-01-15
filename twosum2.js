4f7e4eeb8c4a90b1a9871eeb96db7976b4bea193
JasonRose95 <jasonrosenthalr@gmail.com>
2022-01-14 19:19:56 -0800

    twosum hashmap

---
+++ twosum2.js
@@ -0,0 +1,12 @@
+function twoNumberSum(array, targetSum) {
+  const nums = {};
+  for (const num of array) {
+    const potentialMatch = targetSum - num;
+    if (potentialMatch in nums) {
+      return [potentialMatch, num];
+    } else {
+      nums[num] = true;
+    }
+  }
+  return [];
+}
