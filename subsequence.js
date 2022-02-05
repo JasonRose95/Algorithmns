370cadad0f7193c809d886574ae67e0735f669f4
JasonRose95 <jasonrosenthalr@gmail.com>
2022-02-04 22:13:48 -0800

    subsequence

---
+++ subsequence.js
@@ -0,0 +1,13 @@
+function isValidSubsequence(array, sequence) {
+  // Write your code here.
+  let arrIdx = 0;
+  let seqIdx = 0;
+  while (arrIdx < array.length && seqIdx < sequence.length) {
+    if (array[arrIdx] === sequence[seqIdx]) seqIdx++;
+    arrIdx++;
+  }
+  return seqIdx === sequence.length;
+}
+
+// Do not edit the line below.
+exports.isValidSubsequence = isValidSubsequence;
